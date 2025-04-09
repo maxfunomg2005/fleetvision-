783880a45ab8b0e809a1261f6d6db75186d9e3f2
maxfunomg2005 <116434478+maxfunomg2005@users.noreply.github.com>
2025-04-09 12:58:32 +0500

    Update dashboard-stats.tsx

--- components/dashboard-stats.tsx
+++ components/dashboard-stats.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import { cn } from "@/lib/utils";
 import { useState, useEffect } from "react"
 import { ArrowDownIcon, ArrowRightIcon, ArrowUpIcon, TruckIcon } from "lucide-react"
 
