85d6a110dcc6a6c5b1891831b3cb8d46447c4495
maxfunomg2005 <116434478+maxfunomg2005@users.noreply.github.com>
2025-04-09 12:35:08 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import path from 'path';
 /** @type {import('next').NextConfig} */
 const config = {
   webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname);
+    config.resolve.alias['@'] = path.resolve(__dirname, 'lib');
     return config;
   },
 };
